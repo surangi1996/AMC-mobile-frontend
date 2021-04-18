ff81e8d5af696b5531c1c200db2becc02b03b6cf
surangi1996 <55706550+surangi1996@users.noreply.github.com>
2021-04-18 09:41:44 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,20 @@
-// import 'package:amc/screens/login.dart';
-// import 'package:amc/screens/home_page.dart';
-
-import 'package:amc_new/dashboard.dart';
-import 'package:amc_new/editprofile.dart';
-// import 'package:amc_new/home_page.dart';
-import 'package:amc_new/locate_us.dart';
-import 'package:amc_new/my_amc.dart';
-import 'package:amc_new/my_bills.dart';
-import 'package:amc_new/my_profile.dart';
-import 'package:amc_new/notifications.dart';
-import 'package:amc_new/sign_in.dart';
-import 'package:amc_new/support.dart';
+import 'package:amc_new/screens/dashboard.dart';
+import 'package:amc_new/screens/editprofile.dart';
+import 'package:amc_new/screens/forgotpassword.dart';
+import 'package:amc_new/screens/my_amc.dart';
+import 'package:amc_new/screens/my_bills.dart';
+import 'package:amc_new/screens/my_profile.dart';
+import 'package:amc_new/screens/notifications.dart';
+import 'package:amc_new/screens/reset_password.dart';
+import 'package:amc_new/screens/sign_in.dart';
+import 'package:amc_new/screens/support.dart';
 import 'package:flutter/material.dart';
-
-import 'reports.dart';
-
-// import 'screens/dashboard.dart';
-// import 'screens/dashboard.dart';
-// import 'screens/edit_profile.dart';
-// import 'screens/home_page.dart';
-// import 'screens/locate_us.dart';
-// import 'screens/login_page_background.dart';
-// import 'screens/manage_password.dart';
-// import 'screens/myamc.dart';
-// import 'screens/profile.dart';
-// import 'screens/reportButton.dart';
-// import 'screens/support.dart';
-// import 'screens/sign_in.dart';
-//import 'package:flutter_login_ui/screens/login_screen.dart';
+import 'screens/bills.dart';
+import 'screens/reports.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,20 +22,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Signin(),
       routes: {
-        // '/home_page': (context) => HomePage(),
         '/report': (context) => Reports(),
         '/myamc': (context) => MyAmc(),
         '/myprofile': (context) => Profile(),
-        //   '/editprofile': (context) => EditProfile(),
-        //   '/managepassword': (context) => ManagePassword(),
         '/suport': (context) => Support(),
-        '/locateus': (context) => Locateus(),
         '/signin': (context) => Signin(),
         '/bills': (context) => MyBills(),
         '/dashboard': (context) => Dashboard(),
         '/notifications': (context) => Notifications(),
-        // '/HomePage': (context) => HomePage(),
         '/editprofile': (context) => EditProfile(),
+        '/billreport': (context) => Bills(),
+        '/forgetpassword': (contex) => ForgotPassword(),
+        '/resetpassword': (contex) => ResetPassword(),
       },
     );
   }
